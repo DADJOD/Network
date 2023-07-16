1cb3f128d8d3f3f32184e708fce768233c12265d
DADJOD <kamu57@mail.ru>
2023-07-16 18:30:34 +0300

    connected info
    info with what network we connected

--- app/src/main/java/com/example/network/MainActivity.kt
+++ app/src/main/java/com/example/network/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.network
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 
@@ -18,7 +19,25 @@ class MainActivity : AppCompatActivity() {
         val networkInfo = connectivityManager.activeNetworkInfo
 
         if (networkInfo != null && networkInfo.isConnected) {
-            Toast.makeText(this, "Connected!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
+
+            when(networkInfo.type) {
+                ConnectivityManager.TYPE_WIFI -> {
+                    Log.d("happySDK", "TYPE_WIFI")
+                    Toast.makeText(this, "Connected with WIFI", Toast.LENGTH_SHORT).show()
+                }
+
+                ConnectivityManager.TYPE_MOBILE -> {
+                    Log.d("happySDK", "TYPE_MOBILE")
+                    Toast.makeText(this, "Connected with MOBILE ETHERNET", Toast.LENGTH_SHORT).show()
+                }
+                
+                else ->
+                    Log.d("happySDK", "Never mind")
+            }
+        }
+        else {
+            Toast.makeText(this, "Not connected", Toast.LENGTH_SHORT).show()
         }
     }
 }
