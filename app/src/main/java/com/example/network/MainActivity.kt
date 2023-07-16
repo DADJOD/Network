1febfbb177a7aa55d717d4245b389cb7a3fdd6e6
DADJOD <kamu57@mail.ru>
2023-07-16 18:06:54 +0300

    commit_2

--- app/src/main/java/com/example/network/MainActivity.kt
+++ app/src/main/java/com/example/network/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.network
 
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun checkNetwork(view: View) {
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        if (networkInfo != null && networkInfo.isConnected) {
+            Toast.makeText(this, "Connected!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
